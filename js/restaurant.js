73dff8b103aed2a9d5e440911aefa6400e2afd5c
franco948 <franco.palermo812@gmail.com>
2019-09-28 10:53:09 -0300

    Refactorizar restaurant

--- js/restaurant.js
+++ js/restaurant.js
@@ -9,12 +9,7 @@ var Restaurant = function(id, nombre, rubro, ubicacion, horarios, imagen, califi
 }
 
 Restaurant.prototype.reservarHorario = function(horarioReservado) {
-    for (var i = 0; i < this.horarios.length; i++) {
-        if (this.horarios[i] === horarioReservado) {
-            this.horarios.splice(i, 1);
-            return;
-        }
-    }
+    this.horarios = this.horarios.filter((horario) => horario != horarioReservado);
 }
 
 Restaurant.prototype.calificar = function(nuevaCalificacion) {
@@ -23,15 +18,27 @@ Restaurant.prototype.calificar = function(nuevaCalificacion) {
     }
 }
 
+function sumatoria(numeros)
+{
+    var sumatoria = 0;
+    
+    for (var i = 0; i < numeros.length; i++) {
+        sumatoria += numeros[i]
+    } 
+
+    return sumatoria;
+}
+
+function promedio(numeros)
+{
+    return sumatoria(numeros) / numeros.length;
+}
+
 Restaurant.prototype.obtenerPuntuacion = function() {
     if (this.calificaciones.length === 0) {
         return 0;
     } else {
-        var sumatoria = 0;
-        for (var i = 0; i < this.calificaciones.length; i++) {
-            sumatoria += this.calificaciones[i]
-        }
-        var promedio = sumatoria / this.calificaciones.length;
+        var promedio = promedio(this.calificaciones);
         return Math.round(promedio * 10) / 10;
     }
 
